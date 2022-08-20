1c2314f79d656681937258bcc76406383a8bbe44
unknown <shahzeb.naqvi12@gmail.com>
2022-08-20 22:58:21 +0500

    practice heroku deployment

--- djangoapi/settings.py
+++ djangoapi/settings.py
@@ -82,9 +82,9 @@ WSGI_APPLICATION = 'djangoapi.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
