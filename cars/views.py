effbd7605f44ef0cfe262df71dee0e33e76e2f57
unknown <shahzeb.naqvi12@gmail.com>
2022-08-21 12:47:47 +0500

    practice heroku deployment

--- cars/views.py
+++ cars/views.py
@@ -1,12 +1,15 @@
 from http.client import responses
 from urllib import response
 from django.shortcuts import render
+from django.http import HttpResponse
+
 from rest_framework.generics import ListAPIView
 from rest_framework.generics import CreateAPIView
 from rest_framework.generics import DestroyAPIView
 from rest_framework.generics import UpdateAPIView
-from cars.serializers import TodoSerializer
-from cars.models import Todo
+from cars.serializers import TodoSerializer,ImageSerializer
+from cars.models import Todo,UploadImageTest
+import json
 
 # Create your views here.
 class ListTodoAPIView(ListAPIView):
@@ -27,3 +30,11 @@ class DeleteTodoAPIView(DestroyAPIView):
     """This endpoint allows for deletion of a specific Todo from the database"""
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
+class ImageViewSet(ListAPIView):
+    queryset = UploadImageTest.objects.all()
+    serializer_class = ImageSerializer
+
+    def post(self, request, *args, **kwargs):
+        file = request.data['file']
+        image = UploadImageTest.objects.create(image=file)
+        return HttpResponse(json.dumps({'message': "Uploaded"}), status=200)
